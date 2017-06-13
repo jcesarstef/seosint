9349053b38d461e68de8e4f3d3ec879acc98153c
Julio C. Stefanutto <jcesarstef@gmail.com>
2017-06-13 13:42:30 -0300

    Create seosint.sh

--- seosint.sh
+++ seosint.sh
@@ -10,7 +10,7 @@ more=$(echo $out | tr " " "\n" | base64 -d|grep -o "<span style=\"display:block;
 while [ "$more" != "" ] 
 do
     start=$(($start+100))
-    out=$(curl -A 'Mozilla/5.0 (MSIE; Windows 10)'  "https://www.google.com.br/search?q=$1&num=100&newwindow=1&&start=$start" -sL | base64)
+    out=$(curl -A 'Mozilla/5.0 (MSIE; Windows 10)'  "https://www.google.com.br/search?q=site:$1&num=100&newwindow=1&&start=$start" -sL | base64)
     echo $out | tr " " "\n" | base64 -d | grep -o "href=\"/url?q=\S*" | grep -v webcache.googleusercontent | cut -d "=" -f 3- | cut -d ";" -f 1 | sed 's/&amp$//g'
     more=$(echo $out | tr " " "\n" | base64 -d | grep -o "<span style=\"display:block;margin-left:53px\">\S*</span>")
 done
